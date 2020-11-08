5a791c72adfac1c21e979c29d15c0ec7f147a7fe
Mark Sanders <mark.a.sanders@capgemini.com>
2020-11-08 11:48:17 +0000

    Split write() and end()

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ http.createServer(function server(req, res) {
     res.writeHead(200, { 
         'Content-Type': 'text/plain'
     });
-    res.end('Hello, World!');
+    res.write('Hello, World!');
+    res.end();
 }).listen(3000);
 
 console.log('Server running at http://localhost:3000/');
