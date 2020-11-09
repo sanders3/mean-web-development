8e3143ea84f22788055b22b05db6edde65edd063
Mark Sanders <mark.a.sanders@capgemini.com>
2020-11-09 10:04:21 +0000

    Add logger and chain middleware

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const connect = require('connect');
 
 const app = connect();
 
+const logger = function logger (req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
+
 const helloWorld = function helloWorld (req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!');
 };
 
+app.use(logger);
 app.use(helloWorld);
 app.listen(3000);
 
